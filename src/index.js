d968c9462aae662f249e0e4488a16625123b80f3
Nansamba Ssensalo <nssensalo@gmail.com>
2023-06-08 21:37:03 +0000

    Update index.js and styles.css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
-const currentYear = new Date().getFullYear();
-const name = "Nansamba Ssensalo";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+}
+
+
 ReactDOM.render(
-  <div>
-    <h1 className="header">Hey There</h1>
-    <p>copyright {currentYear}</p>
-    <p>created by {name}</p>
-  </div>,
+  <h1 style={customStyle} >Hey There</h1>,
+//   <div>
+//     <h1 className="header">Favorite Foods</h1>
+//     <div>
+//       <img className="food-img" src="https://images.pexels.com/photos/1460872/pexels-photo-1460872.jpeg?cs=srgb&dl=pexels-engin-akyurt-1460872.jpg&fm=jpg"/>
+//       <img className="food-img" src="https://www.allrecipes.com/thmb/5JVfA7MxfTUPfRerQMdF-nGKsLY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg"/>
+//       <img className="food-img" src="https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg"/> 
+//     </div>
+    
+//  </div>,
 
   document.getElementById("root")
 );
